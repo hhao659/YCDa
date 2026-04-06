4e603ad80aeb88aab91aa1245332f552d3952050
UltralyticsAssistant <web@ultralytics.com>
2026-04-06 04:40:36 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/modules/ESSamp.py
+++ ultralytics/nn/modules/ESSamp.py
@@ -1,15 +1,14 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 
 class DSConv(nn.Module):  # EnhancedDepthwiseConv
     def __init__(self, c1, c2, k=3, s=1, act=True, depth_multiplier=2):
-        super(DSConv, self).__init__()
+        super().__init__()
 
         self.block = nn.Sequential(
-            nn.Conv2d(c1, c1*depth_multiplier, kernel_size=k, stride=s, padding=k//2, groups=c1, bias=False),
+            nn.Conv2d(c1, c1 * depth_multiplier, kernel_size=k, stride=s, padding=k // 2, groups=c1, bias=False),
             nn.BatchNorm2d(c1 * depth_multiplier),
-            nn.GELU() if act else nn.Identity()
+            nn.GELU() if act else nn.Identity(),
         )
 
     def forward(self, x):
@@ -18,12 +17,10 @@ class DSConv(nn.Module):  # EnhancedDepthwiseConv
 
 class ESSamp(nn.Module):
     def __init__(self, c1, c2, k=3, s=1, act=True, depth_multiplier=2):
-        super(ESSamp, self).__init__()
-        self.dsconv = DSConv(c1 * 4, c2, k=k, s=s, act=act,depth_multiplier=depth_multiplier)
+        super().__init__()
+        self.dsconv = DSConv(c1 * 4, c2, k=k, s=s, act=act, depth_multiplier=depth_multiplier)
         self.slices = nn.PixelUnshuffle(2)
-
 
     def forward(self, x):
         x = self.slices(x)
         return self.dsconv(x)
-
